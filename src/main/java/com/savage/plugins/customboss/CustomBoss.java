10db4f1dfdc35a8e8e341eef64f9f3cb967abd03
Savagegamez227 <savagegamez227@gmail.com>
2023-10-05 17:02:38 -0400

    Updated to 1.20.1

--- src/main/java/com/savage/plugins/customboss/CustomBoss.java
+++ src/main/java/com/savage/plugins/customboss/CustomBoss.java
@@ -2,19 +2,24 @@ package com.savage.plugins.customboss;
 
 import com.comphenix.protocol.ProtocolLibrary;
 import com.savage.plugins.customboss.commands.BossCommand;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
 public class CustomBoss extends JavaPlugin {
 
-
-
-
+    private static CustomBoss plugin;
+    Logger log = this.getLogger();
 
 
     @Override
     public void onEnable() {
+        plugin = this;
         // Plugin startup logic
         ProtocolLibrary.getProtocolManager();
         getCommand("createboss").setExecutor(new BossCommand());
+        log.info(ColorUtils.translateColorCodes("&a&l[CustomBoss]: &aPlugin Enabled on server version: " + getServer().getVersion()));
 
     }
 
